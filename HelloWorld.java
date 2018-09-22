c246c07e7f21a6aff313711983ad1cc116192f93
Vinyl Scratch <forceunleasedxxx@gmail.com>
2018-09-22 12:32:40 -0700

    Java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main (String [] args) {
+    String WoodenToaster = "Goodbye World";
+      System.out.println(WoodenToaster);
+      
+      double number = 17.0;
+      int number2 = 83;
+      System.out.println((double) number2 / number);
+  }  
+}
